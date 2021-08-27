6a424daa167d278c68a162e827e8103d8643d22c
mfreyipj <you@example.com>
2021-08-27 15:37:34 +0200

    removed old adjacency function

--- 3x3 Grid/grid.py
+++ 3x3 Grid/grid.py
@@ -45,40 +45,8 @@ def getNumberInput():
 
 
 
-# Evaluate the adjacency of the given number to the empty field by comparing their indexes (0/0 and 0/1 e.g.) through different methods
+# Evaluate the adjacency of the given number to the empty field by comparing their indexes (0/0 and 0/1 e.g.) and their rows
 def evaluateAdjacencyToZero(number):
-
-    # Get the indexes of the number and zero
-    i_number = find(grid, number)
-    i_zero = find(grid, 0)
-
-    # Get the sum of the index of both numbers
-    # Examples : 0/0 -> 0, 1/1 -> 2
-    indexSumZero = i_zero[0] + i_zero[1]
-    indexSumNumber = i_number[0] + i_number[1]
-
-    # Check whether the index sums are even
-    # Examples: 0/0 -> even, 1/2 -> uneven
-    evenIndexSumNumber = indexSumNumber % 2 == 0
-    evenIndexSumZero = indexSumZero % 2 == 0
-
-    # Locate the zero in the grid    
-
-    # If the zero is in the middle
-    if(i_zero == (1, 1)):
-        if not evenIndexSumNumber:
-            return True
-    # If the zero is located in a corner
-    elif evenIndexSumZero:
-        if ((i_zero[0] == i_number[0] or i_zero[1] == i_number[1]) and (indexSumZero - indexSumNumber == 1 or indexSumZero - indexSumNumber == -1)):
-            return True
-    else:
-        if ((i_zero[0] == i_number[0] or i_zero[1] == i_number[1]) and (evenIndexSumNumber)):
-            return True
-    print("The entered number is not adjacent to the empty field!")
-    return False
-
-def evaluateAdjacencyToZeroEasy(number):
 
     # Get the indexes of the number and zero
     i_number = find(grid, number)
@@ -138,7 +106,7 @@ def main():
         # request a number to be moved
         number_to_exchange = getNumberInput()
         # if the number is adjacent to the empty field, exchange them
-        if evaluateAdjacencyToZeroEasy(number_to_exchange):
+        if evaluateAdjacencyToZero(number_to_exchange):
             exchange_with_zero(number_to_exchange)
 
     print("You completed the game!")
