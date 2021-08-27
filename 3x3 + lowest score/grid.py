73bda88124b07fe90a3742a278f910904220e83d
mfreyipj <you@example.com>
2021-08-27 16:23:42 +0200

    rearranged repo for clearer structure

--- 3x3 + lowest score/grid.py
+++ 3x3 + lowest score/grid.py
@@ -8,6 +8,16 @@ grid = [[1, 2, 3],
         [7, 8, 0]]
 '''
 
+max_number = len(grid)**2
+
+def reset_grid():
+    grid = [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 0, 15]]
+    return grid
+
+
 def printGrid():
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -34,45 +44,37 @@ def getNumberInput():
         except:
             print("That's not a real number, try again!\n")
         else:
-            if (userInput > 0 and userInput < 9):
+            if (userInput > 0 and userInput < max_number):
                 return userInput
             else:
                 print("This number is not part of the grid, try again! \n")
 
 
 
-# Evaluate the adjacency of the given number to the empty field by comparing their indexes (0/0 and 0/1 e.g.) through different methods
+# Evaluate the adjacency of the given number to the empty field by comparing their indexes (0/0 and 0/1 e.g.) and their rows
 def evaluateAdjacencyToZero(number):
 
     # Get the indexes of the number and zero
     i_number = find(grid, number)
     i_zero = find(grid, 0)
 
+    # Check whether the number is in the same row or same column
+    sameRow = i_zero[0] == i_number[0]
+    sameColumn = i_zero[1] == i_number[1] 
+
+
     # Get the sum of the index of both numbers
     # Examples : 0/0 -> 0, 1/1 -> 2
     indexSumZero = i_zero[0] + i_zero[1]
     indexSumNumber = i_number[0] + i_number[1]
 
-    # Check whether the index sums are even
-    # Examples: 0/0 -> even, 1/2 -> uneven
-    evenIndexSumNumber = indexSumNumber % 2 == 0
-    evenIndexSumZero = indexSumZero % 2 == 0
-
-    # Locate the zero in the grid    
-
-    # If the zero is in the middle
-    if(i_zero == (1, 1)):
-        if not evenIndexSumNumber:
-            return True
-    # If the zero is located in a corner
-    elif evenIndexSumZero:
-        if ((i_zero[0] == i_number[0] or i_zero[1] == i_number[1]) and (indexSumZero - indexSumNumber == 1 or indexSumZero - indexSumNumber == -1)):
-            return True
+    if((indexSumZero - indexSumNumber == 1 or indexSumZero - indexSumNumber == -1) and (sameRow or sameColumn)):
+        return True
     else:
-        if ((i_zero[0] == i_number[0] or i_zero[1] == i_number[1]) and (evenIndexSumNumber)):
-            return True
-    print("The entered number is not adjacent to the empty field!")
-    return False
+        print("The entered number is not adjacent to the empty field!\n")
+        return False
+
+
 
 # Exchange a given number with the zero in the grid
 def exchange_with_zero(number):
@@ -84,7 +86,7 @@ def exchange_with_zero(number):
     # Exchange the two numbers
     grid[new_i_zero[0]][new_i_zero[1]] = 0
     grid[new_i_number[0]][new_i_number[1]] = number
-
+    
     print("You have successfully moved " + str(number) + " to the empty field!\n")
 
 
@@ -93,28 +95,51 @@ def evaluateGrid(g):
    expected_number = 1   
    for i in range(len(g)):
         for j in range(len(g[i])):
-            if int(g[i][j]) == expected_number or expected_number == 9:
+            if int(g[i][j]) == expected_number or expected_number == max_number:
                 expected_number += 1
             else:
                 return False    
    return True
 
+def playGame():
+    moveCount = 0
+    grid = reset_grid()
+    while(evaluateGrid(grid) != True):
+            printGrid()
+            # request a number to be moved
+            number_to_exchange = getNumberInput()
+            # if the number is adjacent to the empty field, exchange them
+            if evaluateAdjacencyToZero(number_to_exchange):
+                exchange_with_zero(number_to_exchange)
+                moveCount = moveCount + 1
+                print(moveCount)
+    
+    return moveCount
 
 def main():
 
     # Welcome user
     print("Welcome to the game! \n")
+    
+    lowestMoveCount = -1
 
-    while(evaluateGrid(grid) != True):
-        printGrid()
-        # request a number to be moved
-        number_to_exchange = getNumberInput()
-        # if the number is adjacent to the empty field, exchange them
-        if evaluateAdjacencyToZero(number_to_exchange):
-            exchange_with_zero(number_to_exchange)
+    for i in range(3):
+        print("It's your turn Player " + str(i) + "!")
+        # First player set's the first official score
+        if lowestMoveCount == -1:
+            
+            lowestMoveCount = playGame()
+            print("New lowest move count")
+            print(lowestMoveCount)
+        else:
+            newMoveCount = playGame()
+            # replace lowest move count with new move count if it was lower
+            if (newMoveCount < lowestMoveCount):
+                lowestMoveCount = newMoveCount
 
-    print("You completed the game!")
-    quit()
 
+    print("You completed the game!")  
+    print("The lowest move count was " + str(lowestMoveCount)) 
+    quit()    
 
-main() 
+main()
--- 4x4 Grid/grid.py
+++ 4x4 Grid/grid.py
@@ -1,19 +1,21 @@
-grid = [[1, 8, 2],
-        [4, 3, 5],
-        [7, 6, 0]]
-
-''' Control grid -> to test the evaluate function
-grid = [[1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 0]]
+grid = [[15, 2, 1, 12],
+        [8, 5, 6, 11],
+        [4, 9, 10, 7],
+        [3, 14, 13, 0]]
+''' # Control grid -> to test the evaluate function
+grid = [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 0]]
 '''
+
+max_number = len(grid)**2 - 1
 
 def printGrid():
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             print(grid[i][j], end=" ")
         print()
-
 
 # Get index tuple of a number in the grid
 def find(l, elem):
@@ -25,7 +27,6 @@ def find(l, elem):
         return row, column
     return -1
 
-
 # Request a number input from the user and evaluate it as a number
 def getNumberInput():
     while True:
@@ -34,77 +35,59 @@ def getNumberInput():
         except:
             print("That's not a real number, try again!\n")
         else:
-            if (userInput > 0 and userInput < 9):
+            if (userInput > 0 and userInput < max_number):
                 return userInput
             else:
                 print("This number is not part of the grid, try again! \n")
 
-
-
-# Evaluate the adjacency of the given number to the empty field by comparing their indexes (0/0 and 0/1 e.g.) through different methods
+# Evaluate the adjacency of the given number to the empty field by comparing their indexes (0/0 and 0/1 e.g.) and their rows
 def evaluateAdjacencyToZero(number):
 
     # Get the indexes of the number and zero
     i_number = find(grid, number)
     i_zero = find(grid, 0)
 
+    # Check whether the number is in the same row or same column
+    sameRow = i_zero[0] == i_number[0]
+    sameColumn = i_zero[1] == i_number[1] 
+
     # Get the sum of the index of both numbers
     # Examples : 0/0 -> 0, 1/1 -> 2
     indexSumZero = i_zero[0] + i_zero[1]
     indexSumNumber = i_number[0] + i_number[1]
 
-    # Check whether the index sums are even
-    # Examples: 0/0 -> even, 1/2 -> uneven
-    evenIndexSumNumber = indexSumNumber % 2 == 0
-    evenIndexSumZero = indexSumZero % 2 == 0
-
-    # Locate the zero in the grid    
-
-    # If the zero is in the middle
-    if(i_zero == (1, 1)):
-        if not evenIndexSumNumber:
-            return True
-    # If the zero is located in a corner
-    elif evenIndexSumZero:
-        if ((i_zero[0] == i_number[0] or i_zero[1] == i_number[1]) and (indexSumZero - indexSumNumber == 1 or indexSumZero - indexSumNumber == -1)):
-            return True
+    if((indexSumZero - indexSumNumber == 1 or indexSumZero - indexSumNumber == -1) and (sameRow or sameColumn)):
+        return True
     else:
-        if ((i_zero[0] == i_number[0] or i_zero[1] == i_number[1]) and (evenIndexSumNumber)):
-            return True
-    print("The entered number is not adjacent to the empty field!")
-    return False
+        print("The entered number is not adjacent to the empty field!\n")
+        return False
 
 # Exchange a given number with the zero in the grid
 def exchange_with_zero(number):
-
     # Get the new indexes of the number and zero
     new_i_zero = find(grid, number)
     new_i_number = find(grid, 0)
-
     # Exchange the two numbers
     grid[new_i_zero[0]][new_i_zero[1]] = 0
     grid[new_i_number[0]][new_i_number[1]] = number
-
+    
     print("You have successfully moved " + str(number) + " to the empty field!\n")
-
 
 # Evaluate whether the grid was successfully rearranged
 def evaluateGrid(g):
    expected_number = 1   
    for i in range(len(g)):
         for j in range(len(g[i])):
-            if int(g[i][j]) == expected_number or expected_number == 9:
+            if int(g[i][j]) == expected_number or expected_number == max_number+1:
                 expected_number += 1
             else:
                 return False    
    return True
-
-
+   
 def main():
-
     # Welcome user
     print("Welcome to the game! \n")
-
+    
     while(evaluateGrid(grid) != True):
         printGrid()
         # request a number to be moved
@@ -114,7 +97,5 @@ def main():
             exchange_with_zero(number_to_exchange)
 
     print("You completed the game!")
-    quit()
-
-
-main() 
+    quit()    
+main()
